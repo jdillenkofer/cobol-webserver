c99ee8e09da93fefc960c2d0f1c9dcd0ea504f26
Jan Dillenkofer <Jan.Dillenkofer@t-online.de>
2023-11-24 16:38:23 +0100

    feat: Add macos support
    * Emulate sendfile64 syscall by read and write syscalls
    * Dynamically disable signal handling on macos

--- src/file-helper.c
+++ src/file-helper.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <limits.h>
 #include <string.h>
+#include <assert.h>
 
 int is_directory(int fd){
   struct stat fileInfo;
@@ -32,3 +33,49 @@ int is_filepath_subpath_of_cwd(char* filepath) {
   free((void*) cwd);
   return cwd_is_prefix_of_filepath;
 }
+
+int is_macos() {
+  #ifdef __APPLE__
+  return 1;
+  #else
+  return 0;
+  #endif
+}
+
+#ifdef __APPLE__
+ssize_t sendfile64(int out_fd, int in_fd, off_t* offset, size_t count) {
+  // We always use NULL for offset
+  // No need to support the other variant
+  assert(offset != NULL);
+
+  #define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+  #define BUFFERSIZE 4096
+  char buffer[BUFFERSIZE];
+  ssize_t totalBytesWritten = 0;
+  while (totalBytesWritten < count) {
+    ssize_t bytesRead = read(in_fd, buffer, MIN(BUFFERSIZE - totalBytesWritten, BUFFERSIZE));
+  #undef BUFFERSIZE
+  #undef MIN
+    if (bytesRead == 0) {
+      return totalBytesWritten;
+    }
+    if (bytesRead == -1) {
+      return bytesRead;
+    }
+
+    ssize_t totalLoopBytesWritten = 0;
+    do {
+      ssize_t bytesWritten = write(out_fd, buffer + totalLoopBytesWritten, bytesRead - totalLoopBytesWritten);
+      if (bytesWritten == -1) {
+        return bytesWritten;
+      }
+      totalLoopBytesWritten += bytesWritten;
+    }
+    while (bytesRead != totalLoopBytesWritten);
+    
+    totalBytesWritten += totalLoopBytesWritten;
+  }
+  return totalBytesWritten;
+}
+#endif
